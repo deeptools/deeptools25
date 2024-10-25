49952e514f814e49264a411eabe94424b1005f69
adRn-s <salatino@omics.dev>
2024-10-25 13:27:49 +0200

    split boxplots into 2 files

--- scripts/bench_plot.py
+++ scripts/bench_plot.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
+# import numpy as np
 import sys
 
-# Function to read and parse the benchmark.txt file
+
 def read_benchmark(file_path):
     times = []
     memory = []
@@ -14,25 +15,28 @@ def read_benchmark(file_path):
             memory.append(float(parts[2]))  # 'max_rss' column for memory usage
     return times, memory
 
-# Read data from benchmark.txt using first argument
-times, memory = read_benchmark(sys.argv[1])
 
-# Plotting
-fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+def make_boxplot(data, title, ylabel):
+    fig, ax = plt.subplots(figsize=(6, 5))
+    bp = ax.boxplot([data], patch_artist=True)
+    # ax.plot([1], [np.mean(data)], marker='*', color='red', markersize=10, label='Mean')
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    # ax.legend()
+    plt.setp(bp['boxes'], facecolor='lightblue', alpha=0.7)
+    plt.setp(bp['medians'], color='navy')
+    plt.setp(bp['whiskers'], color='navy')
+    plt.setp(bp['caps'], color='navy')
+    plt.setp(bp['fliers'], marker='o', markerfacecolor='red', alpha=0.5)
+    return fig
 
-# Time plot
-ax[0].bar(range(len(times)), times, color='blue')
-ax[0].set_title('Average Execution Time')
-ax[0].set_xlabel('Run')
-ax[0].set_ylabel('Time (s)')
+if __name__ == '__main__':
+    times, memory = read_benchmark(sys.argv[1])
+    output_template = sys.argv[2]
 
-# Memory plot
-ax[1].bar(range(len(memory)), memory, color='green')
-ax[1].set_title('Memory Usage')
-ax[1].set_xlabel('Run')
-ax[1].set_ylabel('Memory (MB)')
+    time_fig = make_boxplot(times, 'Execution Time Distribution', 'Time (s)')
+    time_fig.savefig(output_template.replace('.png', '_time.png'))
 
-plt.tight_layout()
+    mem_fig = make_boxplot(memory, 'Memory Usage Distribution', 'Memory (MB)')
+    mem_fig.savefig(output_template.replace('.png', '_mem.png'))
 
-# Save the plot using second argument
-plt.savefig(sys.argv[2])

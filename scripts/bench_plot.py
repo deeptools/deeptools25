d0e23d515658488df1d9152de7130ef8d07f0bec
adRn-s <salatino@omics.dev>
2024-10-25 13:50:25 +0200

    almost there, but snakefile is buggy

--- scripts/bench_plot.py
+++ scripts/bench_plot.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
-# import numpy as np
 import sys
-
 
 def read_benchmark(file_path):
     times = []
@@ -15,15 +13,15 @@ def read_benchmark(file_path):
             memory.append(float(parts[2]))  # 'max_rss' column for memory usage
     return times, memory
 
-
-def make_boxplot(data, title, ylabel):
-    fig, ax = plt.subplots(figsize=(6, 5))
-    bp = ax.boxplot([data], patch_artist=True)
-    # ax.plot([1], [np.mean(data)], marker='*', color='red', markersize=10, label='Mean')
+def make_boxplot(data1, data2, title, ylabel):
+    fig, ax = plt.subplots(figsize=(8, 5))
+    bp = ax.boxplot([data1, data2], patch_artist=True, labels=['bamCoverage1', 'bamCoverage2'])
     ax.set_title(title)
     ax.set_ylabel(ylabel)
-    # ax.legend()
-    plt.setp(bp['boxes'], facecolor='lightblue', alpha=0.7)
+    colors = ['lightblue', 'lightgreen']
+    for patch, color in zip(bp['boxes'], colors):
+        patch.set_facecolor(color)
+        patch.set_alpha(0.7)
     plt.setp(bp['medians'], color='navy')
     plt.setp(bp['whiskers'], color='navy')
     plt.setp(bp['caps'], color='navy')
@@ -31,12 +29,11 @@ def make_boxplot(data, title, ylabel):
     return fig
 
 if __name__ == '__main__':
-    times, memory = read_benchmark(sys.argv[1])
-    output_template = sys.argv[2]
-
-    time_fig = make_boxplot(times, 'Execution Time Distribution', 'Time (s)')
+    output_template = sys.argv[1]
+    times1, memory1 = read_benchmark(sys.argv[2])
+    times2, memory2 = read_benchmark(sys.argv[3])
+    time_fig = make_boxplot(times1, times2, 'Execution Time Comparison', 'Time (s)')
     time_fig.savefig(output_template.replace('.png', '_time.png'))
-
-    mem_fig = make_boxplot(memory, 'Memory Usage Distribution', 'Memory (MB)')
+    mem_fig = make_boxplot(memory1, memory2, 'Memory Usage Comparison', 'Memory (MB)')
     mem_fig.savefig(output_template.replace('.png', '_mem.png'))
 

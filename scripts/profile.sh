43e69c10e88d448cbb91043ed013315c8a74e3ee
adRn-s <salatino@omics.dev>
2024-10-28 11:47:46 +0100

    rerun today made me adjust these

--- scripts/profile.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-PATHROOT=/data/manke/processing/salatino/deepdev/deeptools25/work-dir/testfiles
-samply record bamCoverage2 -b ${PATHROOT}/S2_H2AZ.bam \
-    -o ${PATHROOT}/output/new.bg -of bedgraph -bs 1 -p 1

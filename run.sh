89304eb11f53e29a99df4a769747d958b643f3ac
adRn-s <lims@omics.dev>
2025-02-12 10:35:17 +0100

    fix empty log files (maybe)

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 mkdir -p ./{output,logs} && rm -rf ./{output,logs}/*
-git pull && snakemake --use-conda \
+git pull && snakemake --use-conda --latency-wait 30 \
     --forcerun --benchmark-extended \
     --cores $(nproc --all)

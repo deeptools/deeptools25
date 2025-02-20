c98598c575bb228c76291bd2981c4400c2b12f3e
adRn-s <salatino@omics.dev>
2025-02-20 12:38:50 +0100

    smartypants

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 mkdir -p ./{output,logs} && rm -rf ./{output,logs}/*
-git pull && snakemake --use-conda --latency-wait 30 \
-    --forcerun --benchmark-extended \
-    --cores $(nproc --all)
+git pull &&
+	snakemake --use-conda --forcerun --benchmark-extended \
+		--cores $(awk -F'=' '/Nthreads/ {print $2; exit}' Snakefile)

1e9877acacfdfd04973863d73a161cb7bc794c8b
adRn-s <lims@omics.dev>
2025-03-07 17:23:18 +0100

    makes more sense :p

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-mkdir -p ./{output,logs} && rm -rf ./{output,logs}/*
+rm -rf ./{output,logs} && mkdir -p ./{output,logs}
 #git pull &&
 #	snakemake --use-conda --forcerun --benchmark-extended \
 #		--cores $(awk -F'=' '/Nthreads/ {print $2; exit}' Snakefile)

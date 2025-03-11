1a4d3a53b2408155d8fb0aa2afdbcfcdd2cb1826
adRn-s <lims@omics.dev>
2025-03-11 09:59:50 +0100

    Nthreads=16

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
-mkdir -p .snakemake && find .snakemake -maxdepth 1 -mindepth 1 -not -name conda -type d -delete
-rm -rf ./{output,logs} && mkdir -p ./{output,logs}
+rm -rf trash && mv .snakemake trash
+mkdir .snakemake && mv trash/conda .snakemake/
+mv output trash/ && mv logs trash/
+rm -rf trash
 #git pull && \
 #	snakemake --use-conda --forcerun --benchmark-extended \
 #		--cores $(awk -F'=' '/Nthreads/ {print $2; exit}' Snakefile)
-samtools quickcheck zenodo/*.bam
+samtools quickcheck zenodo/*.bam && sleep 3s
 snakemake --profile snk-slurm-exe --benchmark-extended

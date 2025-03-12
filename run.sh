58460b4dee014f26ddb90265afb683ad1e0f9144
adRn-s <lims@omics.dev>
2025-03-12 17:26:48 +0100

    ah, yeah. this..

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 rm -rf trash && mv .snakemake trash
 mkdir .snakemake && mv trash/conda .snakemake/
-mv output trash/ && mv logs trash/
+mkdir -p {output,logs} && mv output trash/ && mv logs trash/
 rm -rf trash
 #git pull && \
 #	snakemake --use-conda --forcerun --benchmark-extended \

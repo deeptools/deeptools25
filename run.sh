683b2f4d9edba8be243e4315667efd54e687ff1b
adRn-s <lims@omics.dev>
2025-03-13 10:51:43 +0100

    clean

--- run.sh
+++ run.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 set -euo pipefail
+
+# Tidy-up, start fresh
 rm -rf trash && mv .snakemake trash
 mkdir .snakemake && mv trash/conda .snakemake/
 mkdir -p {output,logs} && mv output trash/ && mv logs trash/
 rm -rf trash
+
+## This is how we would run the pipeline on a local machine
 #git pull && \
 #	snakemake --use-conda --forcerun --benchmark-extended \
 #		--cores $(awk -F'=' '/Nthreads/ {print $2; exit}' Snakefile)
+## On an HPC cluster, we need to submit the job to the scheduler (see next block)
+
+# Check input files were transferred okay, then execute the pipeline
 samtools quickcheck zenodo/*.bam && sleep 3s
 snakemake --profile snk-slurm-exe --benchmark-extended
+
+# Save everything except for data output files
+find output -mindepth 1 -maxdepth 1 -type d | xargs rm -rf
+mv benchmark_*.log logs/
+mv .snakemake/log/*.snakemake.log logs/
+mv .snakemake/slurm_logs logs/
+mv logs output/
+mv output run_$(date +%Y%m%d%H%M%S)
